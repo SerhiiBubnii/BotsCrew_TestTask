5db1cccba9a2744c9d5d673d7f3fa46e5ccc0b34
Serhii Bubnii <bubniy1995@gmail.com>
2019-11-09 13:58:19 +0200

    update project (code review)

--- src/main/java/com/bubnii/model/entity/Lector.java
+++ src/main/java/com/bubnii/model/entity/Lector.java
@@ -1,8 +1,6 @@
 package com.bubnii.model.entity;
 
 import lombok.*;
-import org.hibernate.search.annotations.*;
-import org.hibernate.search.annotations.Index;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -11,18 +9,15 @@ import java.util.Set;
 @Getter
 @Setter
 @Entity
-@Indexed
 @Table(name = "lector")
 public class Lector {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Field(index = Index.YES, analyze = Analyze.YES, store = Store.NO)
     @Column(name = "first_name")
     private String firstName;
 
-    @Field(index = Index.YES, analyze = Analyze.YES, store = Store.NO)
     @Column(name = "last_name")
     private String lastName;
 
